0c117ae53554abf3db7d7f49c3b27d499a601120
Glitch (glitch-in-bio) <none>
2025-02-14 04:23:22 +0000

    🕷🍨 Checkpoint ./settings.json:74971499/1614 ./templates/footer.js:74971499/1693

--- templates/footer.js
+++ templates/footer.js
@@ -1,7 +1,9 @@
 import { html } from "lighterhtml";
 import settings from "../settings.json";
 
-export const renderFooter = html.node`<footer class="footer">
+export const renderFooter = html.node`
+`;
+<footer class="footer">
   <div class="tagline">
     <b>Glitch in Bio</b><br />
     Your links, your way.
@@ -18,4 +20,3 @@ export const renderFooter = html.node`<footer class="footer">
     Get your own free page
   </a>
 </footer>
-`;
