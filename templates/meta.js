121eaaf9b0ab0848fbd5273b4d91bf61fd0e0e7a
Glitch (cyclic-stellar-transport) <none>
2025-03-07 06:09:24 +0000

    v2 website

--- templates/meta.js
+++ templates/meta.js
@@ -2,10 +2,8 @@ import { html } from "lighterhtml";
 import settings from "../settings.json";
 
 export const renderMeta = html.node`
-  <title>${settings.metaTitle}</title>
 
   <!-- OpenGraph/Social sharing -->
-  <meta name="description" content="${settings.metaDescription}" />
   <meta name="og:description" content="${settings.metaDescription}" />
   <meta property="og:image" content="${settings.avatarImage}" />
   <meta property="og:title" content="${settings.metaTitle}" />
@@ -13,3 +11,15 @@ export const renderMeta = html.node`
   <!-- Load our custom theme -->
   <link rel="stylesheet" type="text/css" href="/styles/themes/${settings.theme}.css" />
 `;
+// `
+//   <title>${settings.metaTitle}</title>
+
+//   <!-- OpenGraph/Social sharing -->
+//   <meta name="description" content="${settings.metaDescription}" />
+//   <meta name="og:description" content="${settings.metaDescription}" />
+//   <meta property="og:image" content="${settings.avatarImage}" />
+//   <meta property="og:title" content="${settings.metaTitle}" />
+//   <meta name="twitter:card" content="summary" />
+//   <!-- Load our custom theme -->
+//   <link rel="stylesheet" type="text/css" href="/styles/themes/${settings.theme}.css" />
+// `
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-
+import path from 'path';
+// import viteCompression from 'vite-plugin-compression';
 export default defineConfig(async ({ command, mode }) => {
   return {
     build: {
@@ -12,10 +13,30 @@ export default defineConfig(async ({ command, mode }) => {
       strictPort: true,
       hmr: {
         clientPort: 443
-      }
+      },
+      fs: {
+        strict: true,
+        allow: [
+          // Add custom allowed paths
+          path.resolve(__dirname, 'public/'),
+          path.resolve(__dirname, 'scripts/'),
+          path.resolve(__dirname, 'templates/'),
+          path.resolve(__dirname, 'index.html'),
+          path.resolve(__dirname, 'settings.json'),
+          path.resolve(__dirname, 'sitemap.xml'),
+          path.resolve(__dirname, 'c1e17fc4d4194878b246934e279744f7.txt')
+        ],
+      },
     },
     optimizeDeps: {
       exclude: ['./settings.json']
-    },
+    }//,
+    // plugins: [
+    //   viteCompression({
+    //     algorithm: 'brotliCompress', // Use 'gzip' for Gzip compression
+    //     threshold: 1024, // Only compress files larger than 1KB
+    //     deleteOriginFile: false, // Keep original files
+    //   })
+    // ]
   };
 });
